a88516b19f0308c17cba523939e843e17c88edef
May1018 <102577582+May1018@users.noreply.github.com>
2022-03-29 15:18:51 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1648538206,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/hole-sheet0.png",
+		"images/hammer-sheet0.png",
+		"images/robot-sheet0.png",
+		"images/robot-sheet1.png",
+		"images/robot-sheet2.png",
+		"media/mixkit-little-cute-kiss-2192.m4a",
+		"media/mixkit-little-cute-kiss-2192.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
